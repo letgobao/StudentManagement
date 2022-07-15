cddce5273a8b5391b9fa5a74709ff81b0396e41f
thanhlong3009 <you@example.com>
2022-07-15 17:13:32 +0700

    class list done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import Login from './page/login/Login';
-import Profile from './page/profile/Profile';
-import Header from './component/header/Header';
-import Achievement from './page/achievement/Achievement';
-import Manager from './page/manager/Manager';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./page/login/Login";
+import Profile from "./page/profile/Profile";
+import Header from "./component/header/Header";
+import Achievement from "./page/achievement/Achievement";
+import Manager from "./page/manager/Manager";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClassList from "./component/classList/classList";
 
 function App() {
   return (
-    <div className="App">  
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login/>}/>    
+          <Route path="/login" element={<Login />} />
         </Routes>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<Manager/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/achievement' element={<Achievement/>}/>
+          <Route path="/" element={<Manager />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/achievement" element={<Achievement />} />
+          <Route path="/classList" element={<ClassList />} />
         </Routes>
       </BrowserRouter>
     </div>
